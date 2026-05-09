c572adec4015e59b275f6761fb73bacfb014494a
Lijo Thomas <lijothomas7777777@gmail.com>
2026-05-10 03:45:59 +0530

    splash animation added

--- android/app/src/main/java/com/activitypointsnative/MainActivity.kt
+++ android/app/src/main/java/com/activitypointsnative/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.activitypointsnative
 
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -7,16 +15,50 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
   override fun getMainComponentName(): String = "ActivityPointsNative"
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    // installSplashScreen() MUST be called before super.onCreate()
+    val splashScreen = installSplashScreen()
+
+    super.onCreate(savedInstanceState)
+
+    // Custom exit animation (Android 12+ only)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+      splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
+        val splashScreenView = splashScreenViewProvider.view
+        val iconView = splashScreenViewProvider.iconView
+
+        val scaleX = ObjectAnimator.ofFloat(iconView, View.SCALE_X, 1f, 0f)
+        val scaleY = ObjectAnimator.ofFloat(iconView, View.SCALE_Y, 1f, 0f)
+        val fadeIcon = ObjectAnimator.ofFloat(iconView, View.ALPHA, 1f, 0f)
+        val fadeBackground = ObjectAnimator.ofFloat(splashScreenView, View.ALPHA, 1f, 0f)
+
+        val iconAnimator = AnimatorSet().apply {
+          playTogether(scaleX, scaleY, fadeIcon)
+          duration = 400
+          interpolator = AnticipateInterpolator()
+        }
+
+        val bgAnimator = AnimatorSet().apply {
+          play(fadeBackground)
+          duration = 500
+          startDelay = 200
+        }
+
+        AnimatorSet().apply {
+          playTogether(iconAnimator, bgAnimator)
+          addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: android.animation.Animator) {
+              splashScreenViewProvider.remove()
+            }
+          })
+          start()
+        }
+      }
+    }
+  }
 }
